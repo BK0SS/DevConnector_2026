622f2d13720b2a4c570ff1ca6ac278c14475ec01
BK0SS <kosulin.b@mail.ru>
2026-02-01 20:47:46 -0800

    posts board

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 if(localStorage.token){
     setAuthToken(localStorage.token);
@@ -92,6 +93,16 @@ const App = () => {
                         </PrivateRoute>
                       } 
                     />
+
+                      <Route 
+                      path="/posts" 
+                      element={
+                        <PrivateRoute>
+                          <Posts />
+                        </PrivateRoute>
+                      } 
+                    />
+
                     <Route path="/profiles" element={<Profiles />} />
                     <Route path="/profile/:id" element={<Profile />} />
                   </Routes>
